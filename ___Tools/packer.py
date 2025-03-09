3a7bfa936af8e953ec781acc83683691cccd6528
JunkIce <claytonwsurber@gmail.com>
2025-03-08 22:41:29 -0700

    Hide Code in packer

--- ___Tools/packer.py
+++ ___Tools/packer.py
@@ -4,10 +4,7 @@
 mode=3
 
 #Put code here
-CODE="""from string import*;
-r=0;
-for c in"t1d0)n}ky/,/Z}:YHIjmRRVQz(ZPgiwAhn(p&$ro+yPYo0dXZ2(ziJ`>G+E_SZWl~jabR@5:|Ea1tb4uC]yX_s}'xn[4r(I]]}=9tRMu%uT9}-w$6w0)ei3xn&+ZD?&v5g6<JFw+@[$DhbN<x*BcP>PgB9I!7rmAq6t,VrSQqC{ CF'JRS0^!+QdTA?mmcqK5LG$Wk;Q-(LPRL2&kpNG3!z_bmqC0{F^q7/_1<Q|'}3PV/wpF@Hlj>2RFFR@i3_{{Ln8n)b*6t$lK%[ :whlre I4,KY))PJL]W1H=NLk>~hM;[P#/X[Rcp,%G^weX[A>jM_pF`0qHb #p!/YU59WfP/}<L?dkk`_[{sr?H:W?hn9~b7 ;#+Ch>,90H7n%z!DdEDTiUOMkaYa'3GC-|`93Q/B0NthI+{&Dn$E_0i=bo?~iV9Ebfb[J#F=~Tv2YR.1s%lF#Od@8{?[b%mIma7x6X=#wnP09oJ+&ga!!Hz4cBq{2`IU*Ss>;CLHU!X":r=r*93+{c:i for i,c in enumerate(digits+ascii_uppercase+ascii_lowercase+"!#$%&'()*+-.:;<=>?@[]^_{|}~`,/ ")}[c]
-print(f"0.{r}")"""
+CODE="""//Your//Code//Here"""
 print()
 
 #1:2 compressor
